eafb6ff89ff1e7752e1830171030f6b8b68da9c7
David Hovemeyer <david.hovemeyer@gmail.com>
2016-11-20 18:04:56 -0500

    add more kinds of tokens

--- src/catparty/clexer.cljs
+++ src/catparty/clexer.cljs
@@ -38,11 +38,61 @@
    "while"
    ])
 
+(def c-identifier-pattern
+  [[#"^[A-Za-z_][A-Za-z0-9_]*" :identifier]])
+
 (def c-keyword-patterns
   (mapv (fn [kw] [(re-pattern (str "^" kw)) (keyword (str "kw_" kw))]) c-keywords))
 
+(def c-operator-patterns
+  [[#"^==" :op_eq]
+   [#"^=" :op_assign]
+   [#"^<<" :op_lshift]
+   [#"^<=" :op_lte]
+   [#"^<" :op_lt]
+   [#"^>>" :op_rshift]
+   [#"^>=" :op_gte]
+   [#"^>" :op_gt]
+   [#"^!=" :op_ne]
+   [#"^!" :op_not]
+   [#"^\|\|" :op_or]
+   [#"^\|" :op_bit_or]
+   [#"^&&" :op_and]
+   [#"^&" :op_bit_and]
+   [#"^\^" :op_bit_xor]
+   [#"^~" :op_bit_compl]
+   [#"^\+" :op_plus]
+   [#"^-" :op_minus]
+   [#"^\*" :op_star]
+   [#"^/" :op_div]
+   [#"^%" :op_mod]
+   ]
+  )
+
+(def c-punct-patterns
+  [[#"^\(" :lparen]
+   [#"^\)" :rparen]
+   [#"^\[" :lbracket]
+   [#"^\]" :rbracket]
+   [#"^\{" :lbrace]
+   [#"^\}" :rbrace]
+   [#"^:" :colon]
+   [#"^;" :semicolon]
+   [#"^," :comma]
+   ])
+
+(def c-literal-patterns
+  [[#"^[0-9]+" :dec_literal]
+   [#"^0[Xx][0-9A-Fa-z]+(UuLl)*" :hex_literal]
+   ]
+  )
+
 (def c-all-patterns
-  (concat c-keyword-patterns))
+  (concat c-keyword-patterns
+          c-identifier-pattern
+          c-operator-patterns
+          c-punct-patterns
+          c-literal-patterns))
 
 (defn create-from-lines
   "Create a C lexer from sequence of lines."
