554fa00d6591a9ca962f382a8bf02ea3b75bc911
David Hovemeyer <david.hovemeyer@gmail.com>
2016-11-27 17:04:30 -0500

    started C parser (working from K&R 2nd ed.)
    Note that some grammar changes will be necessary:
    e.g., all left-recursive productions will need to
    be right-recursive.

--- src/catparty/cparser.cljs
+++ src/catparty/cparser.cljs
@@ -1,2 +1,26 @@
-(ns catparty.cparser)
+(ns catparty.cparser
+  (:require [catparty.parser :as parser])
+  )
+
+(defn parse-external-declaration [token-seq]
+  ; FIXME: this is just a placeholder for now
+  (parser/do-production :external-declaration [(parser/expect :int_literal)] token-seq)
+  )
+
+(defn parse-translation-unit [token-seq]
+  ; Possibilities are:
+  ;    translation-unit -> ^ external-declaration
+  ;    translation-unit -> ^ external-declaration translation-unit
+  (let [startpr (parser/initial-parse-result token-seq)
+        ; Parse a single external declaration
+        pr (parser/apply-production startpr [parse-external-declaration])
+        remaining-tokens (:tokens pr)]
+    (if (empty? remaining-tokens)
+      ; At EOF, end the production here
+      (parser/complete-production :translation-unit pr)
+      ; Not at EOF, continue recursively
+      (parser/complete-production :translation-unit
+                                  (parser/apply-production pr [parse-translation-unit]))
+      )
+  ))
 
