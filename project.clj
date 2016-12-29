dd97b8c566cc503db3445d00ebdccaea18bff376
David Hovemeyer <david.hovemeyer@gmail.com>
2016-12-29 17:04:32 -0500

    use GPL 3 license

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject catparty "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "C parser for Clojure/ClojureScript"
+  :url "https://github.com/daveho/catparty"
+  :license {:name "GNU GPL version 3"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
